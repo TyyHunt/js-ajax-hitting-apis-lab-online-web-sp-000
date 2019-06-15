3c20a4ece6319b2a4aa01604ea85ea73be9c173f
Tyler Hunt <tyhunt04@gmail.com>
2019-06-15 17:12:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,4 +4,8 @@ function getRepositories() {
   const username = document.getElementById('username').value;
   const uri = 'https://api.github.com' + '/users/' + name + '/repos';
   const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', displayRepositories);
+  xhr.open('GET', uri)
+  xhr.send()
 }
