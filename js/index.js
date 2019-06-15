61ff84dbee41c66877ab348840d1acb6035bdd2b
Tyler Hunt <tyhunt04@gmail.com>
2019-06-15 17:22:27 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,30 +1,29 @@
 // your code here
 
 function getRepositories() {
-  const username = document.getElementById('username').value;
-  const uri = 'https://api.github.com' + '/users/' + name + '/repos';
-  const xhr = new XMLHttpRequest();
-
-  xhr.addEventListener('load', displayRepositories);
-  xhr.open('GET', uri)
-  xhr.send()
+	const name = document.getElementById('username').value;
+	const uri = 'https://api.github.com' + '/users/' + name + '/repos';
+	const xhr = new XMLHttpRequest();
+	xhr.addEventListener('load', displayRepositories);
+	xhr.open('GET', uri);
+	xhr.send()
 }
 
- function displayRepositories() {
-   const repos = JSON.parse(this.responseText);
- 	const repoList = `<ul>${repos
- 		.map(r => {
- 			const dataUsername = 'data-username="' + r.owner.login + '"';
- 	        const dataRepoName = 'data-repo="' + r.username + '"';
- 	        return `
- 	        	<li>${r.name}
- 	        	<a href="${r.html_url}">${r.html_url}</a><br>
- 	        	<a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
- 	        	<a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a><br>
- 	        	</li>`}
- 			).join('')}</ul>`;
- 		document.getElementById('repositories').innerHTML = repoList;
- }
+function displayRepositories() {
+	const repos = JSON.parse(this.responseText);
+	const repoList = `<ul>${repos
+		.map(r => {
+			const dataUsername = 'data-username="' + r.owner.login + '"';
+	        const dataRepoName = 'data-repo="' + r.name + '"';
+	        return `
+	        	<li>${r.name}
+	        	<a href="${r.html_url}">${r.html_url}</a><br>
+	        	<a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+	        	<a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a><br>
+	        	</li>`}
+			).join('')}</ul>`;
+		document.getElementById('repositories').innerHTML = repoList;
+}
 
  function getCommits(el) {
 	const repoName = el.dataset.repository;
