44014d97dc954d38751116c1b5fbede5f5cc72ed
Tyler Hunt <tyhunt04@gmail.com>
2019-06-15 17:17:02 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,3 +9,19 @@ function getRepositories() {
   xhr.open('GET', uri)
   xhr.send()
 }
+
+ function displayRepositories() {
+   const repos = JSON.parse(this.responseText);
+ 	const repoList = `<ul>${repos
+ 		.map(r => {
+ 			const dataUsername = 'data-username="' + r.owner.login + '"';
+ 	        const dataRepoName = 'data-repo="' + r.username + '"';
+ 	        return `
+ 	        	<li>${r.name}
+ 	        	<a href="${r.html_url}">${r.html_url}</a><br>
+ 	        	<a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+ 	        	<a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a><br>
+ 	        	</li>`}
+ 			).join('')}</ul>`;
+ 		document.getElementById('repositories').innerHTML = repoList;
+ }
